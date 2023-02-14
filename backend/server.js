94e16649c3a735a8e63b2bd0f0da4114849f9845
Slavy Spasoff <slavyspasoff@gmail.com>
2023-02-14 11:12:40 +0100

    add morgan middleware

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,43 @@
-import express from "express";
-import { connectToDB } from "./config/db_config.js";
-import configureJwtStrategy from "./security/passport-jwt.js";
-import cookieParser from "cookie-parser";
-import passport from "passport";
-import dotenv from "dotenv";
-import cors from "cors";
-import authRouter from "./routers/authRouter.js";
-import userRouter from "./routers/userRouter.js";
-import postRoutes from "./routers/postsRouter.js";
+import express from 'express'
+import { connectToDB } from './config/db_config.js'
+import configureJwtStrategy from './security/passport-jwt.js'
+import cookieParser from 'cookie-parser'
+import passport from 'passport'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import authRouter from './routers/authRouter.js'
+import userRouter from './routers/userRouter.js'
+import postRoutes from './routers/postsRouter.js'
+import morgan from 'morgan'
 
-const app = express();
-dotenv.config();
+const app = express()
+dotenv.config()
 const config = {
-  origin: "http://localhost:8080",
-  credentials: true
-};
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors(config));
+  origin: 'http://localhost:8080',
+  credentials: true,
+}
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors(config))
+app.use(morgan('dev'))
 
 //! use passport and initialize
-app.use(passport.initialize());
+app.use(passport.initialize())
 
-configureJwtStrategy(passport);
+configureJwtStrategy(passport)
 
 /** DB CONNECTION */
-connectToDB();
+connectToDB()
 
 //Routers
 //serve the frontend pix from uploads
-app.use("/uploads", express.static("uploads"));
+app.use('/uploads', express.static('uploads'))
 
-app.use("/api", authRouter);
-app.use("/api/users", userRouter);
-app.use("/api/posts", postRoutes);
-
-
+app.use('/api', authRouter)
+app.use('/api/users', userRouter)
+app.use('/api/posts', postRoutes)
 
 /** SET PORT NUMBER */
-const port = process.env.PORT || 5555;
-app.listen(port, console.log(`Server is up on port ${port} 👻`));
+const port = process.env.PORT || 5555
+app.listen(port, console.log(`Server is up on port ${port} 👻`))
