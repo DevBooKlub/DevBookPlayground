e9cd0907cef1c3d68d552373e06fb4a3c825f538
Slavy Spasoff <slavyspasoff@gmail.com>
2023-02-15 10:21:02 +0100

    add logout method to the auth controller

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -1,20 +1,24 @@
-import express from "express";
-import upload from "../config/multer.js";
+import express from 'express'
+import upload from '../config/multer.js'
 import {
   Register,
   Login,
   verification,
-} from "../controllers/authController.js";
+  logout,
+} from '../controllers/authController.js'
 
-const router = express.Router();
+const router = express.Router()
 
 /** register */
-router.post("/register", upload.single("userPic"), Register);
+router.post('/register', upload.single('userPic'), Register)
 
 /** login */
-router.post("/login", Login);
+router.post('/login', Login)
+
+/** logout */
+router.post('/logout', logout)
 
 /** verify account */
-router.route("/verify/email/:userid/:verifytoken").get(verification);
+router.route('/verify/email/:userid/:verifytoken').get(verification)
 
-export default router;
+export default router
