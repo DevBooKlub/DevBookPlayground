178cb3abeb84a834fd2a80fbf0ec84a9cb80dd69
Denis <denimcardle@gmail.com>
2023-02-20 21:34:41 +0100

    backend cleaned and coded on feature/den-cleanup

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,19 @@
-import express from 'express'
-import { connectToDB } from './config/db_config.js'
-import configureJwtStrategy from './security/passport-jwt.js'
-import cookieParser from 'cookie-parser'
-import passport from 'passport'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import authRouter from './routers/authRouter.js'
-import userRouter from './routers/userRouter.js'
-import postRoutes from './routers/postsRouter.js'
-import morgan from 'morgan'
-
-const app = express()
-dotenv.config()
-const config = {
-  origin: 'http://localhost:8080',
-  credentials: true,
-}
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors(config))
-app.use(morgan('dev'))
-
-//! use passport and initialize
-app.use(passport.initialize())
-
-configureJwtStrategy(passport)
-
-/** DB CONNECTION */
-connectToDB()
-
-//Routers
-//serve the frontend pix from uploads
-app.use('/uploads', express.static('uploads'))
-
-app.use('/api', authRouter)
-app.use('/api/users', userRouter)
-app.use('/api/posts', postRoutes)
-
-/** SET PORT NUMBER */
-const port = process.env.PORT || 5555
-app.listen(port, console.log(`Server is up on port ${port} 👻`))
+import { env } from 'process';
+import mongoose from 'mongoose';
+import './dotenv.init.js';
+import app from './app.js';
+const { PORT, DB_USER, DB_PASS, DB_URL_BASE, BASE_URL } = env;
+const port = Number(PORT) || 5555;
+const DB_URL = DB_URL_BASE.replace('<username>', DB_USER).replace('<password>', DB_PASS);
+mongoose.set('strictQuery', false);
+mongoose
+    .connect(DB_URL)
+    .then((M) => {
+    console.log(`Connected to ${M.connection.name} database`);
+})
+    .catch((err) => {
+    console.log(`Connection to the database failed with an error message: \n ${err.message}`);
+});
+const server = app.listen(port, () => {
+    console.log(`Server listening on ${BASE_URL}`);
+});
